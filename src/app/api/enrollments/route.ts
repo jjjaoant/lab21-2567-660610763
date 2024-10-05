c36c33f639c682837003cfdb296ae302c6ff27cf
jjjaoant <antthanchanok25488@gmail.com>
2024-10-05 15:00:01 +0700

    v.1

--- src/app/api/enrollments/route.ts
+++ src/app/api/enrollments/route.ts
@@ -1,5 +1,5 @@
 import { checkToken } from "@lib/checkToken";
-import { Database, Payload } from "@lib/types";
+import { Payload } from "@lib/types";
 import { NextRequest, NextResponse } from "next/server";
 import { getPrisma } from "@lib/getPrisma";
 
@@ -80,8 +80,46 @@ export const POST = async (request: NextRequest) => {
     );
   }
 
-  // Coding in lecture
+  const prisma = getPrisma();
 
+  // Check if course exists
+  const course = await prisma.course.findFirst({ //or findUnique
+    where: { courseNo: courseNo },
+  });
+
+  if(!course){
+    return NextResponse.json(
+        {
+          ok: false,
+          message: "Course number does not exist",
+        },
+        { status: 404 }
+      );
+    }
+
+  //check if course already enrolled
+  const enrolled = await prisma.enrollment.findFirst({
+    where: {
+      courseNo: courseNo,
+      studentId: studentId,
+    },
+  });
+
+  if (enrolled) {
+    return NextResponse.json(
+      { ok: false, message: "You already registered this course" },
+      { status: 400 }
+    );
+  }
+
+  //add course to database (enrollments collection)
+    await prisma.enrollment.create({
+      data: {
+        courseNo: courseNo,
+        studentId: studentId,
+      },
+    })
+  
   return NextResponse.json({
     ok: true,
     message: "You has enrolled a course successfully",
@@ -127,7 +165,15 @@ export const DELETE = async (request: NextRequest) => {
 
   const prisma = getPrisma();
   // Perform data delete
-
+  await prisma.enrollment.delete({
+    where: {
+    courseNo_studentId: {
+      courseNo: courseNo,
+      studentId: studentId,
+      },
+    },
+  });
+  
   return NextResponse.json({
     ok: true,
     message: "You has dropped from this course. See you next semester.",
